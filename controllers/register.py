d23642f78a5d8b8948e6d09968040b6276306849
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-09-23 18:21:27 +0700

    Create register.py

---
+++ controllers/register.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+view = Blueprint('register_view', 
+                    __name__, 
+                    template_folder='templates', 
+                    static_folder='static', 
+                    static_url_path='/static/controllers')
+
+
+@view.route('/register')
+def admin_register():
+    return render_template('register.html')
