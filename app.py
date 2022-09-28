ae669daef470598a8db89b393ffd7fd7da04edb2
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-09-28 13:30:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from pages import dashboard, attendance, add_user, user_list
 
 app = Flask(__name__, static_folder=None)
 
-
 app.register_blueprint(login.view)
 app.register_blueprint(logout.view)
 app.register_blueprint(register.view)
